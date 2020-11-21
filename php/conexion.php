7202c572dec0bc1ff383f695c273050d42598a6a
Leandro <lnmarquez19@gmail.com>
2020-11-21 20:17:15 -0300

    antes de pullear lo del sensei

--- php/conexion.php
+++ php/conexion.php
@@ -2,25 +2,25 @@
 <!----------------- CONEXION A BASE DE DATOS GLOBAL -------------------> 
 
 <?php
-
+/*
 $conexion = mysqli_connect(   //funcion para conectarse a la bd. La guarda en una variable para utilizarla luego
   'johnny.heliohost.org',     //servidor local
   'sensei_1',               	//usuario por defecto
   '12345',                    //sin contraseña
   'sensei_budines'            //nombre bd   
 );
-
+*/
 ?>
 
 <!----------------- CONEXION A BASE DE DATOS LOCAL -------------------> 
 
 <?php
-/*
+
 $conexion = mysqli_connect(    //funcion para conectarse a la bd. La guarda en una variable para utilizarla luego
   'localhost',                 //servidor local
   'root',                   	//usuario por defecto
   '',                         //sin contraseña
   'bd_budines'                 //nombre bd  
 );
-*/
+
 ?>
